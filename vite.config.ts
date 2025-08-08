f06cdee2c50aa8f7669d2fd37b32314124937a97
unknown <admin@techsimple-to.ca>
2025-08-08 10:16:26 -0400

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/techsimple-client-vite/', // <-- repo name
+});
+// This configuration sets up Vite for a React project with the base path set to the repository name.
